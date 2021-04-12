33befdd42902206c2fbd37ca37adde221e3ef568
Noah Schneider <noah.schneider@outlook.com>
2021-04-12 22:14:30 +0200

    Added too many features again...

--- js/script.js
+++ js/script.js
@@ -1,65 +1,115 @@
-let m = 10
-let s = 0
+let timer;
 
-let wrapper = document.getElementById("MyWrapper");
-let clock = document.getElementById("MyClockDisplay");
-let display = document.getElementById("MyTextDisplay");
-let button = document.getElementById("MyStartButton");
+let m_start = 10;
+let s_start = 0;
+
+let m = m_start;
+let s = s_start;
+
+let wrapper = document.getElementById("wrapper");
+let display = document.getElementById("display");
+let clock = document.getElementById("clock");
+let text = document.getElementById("text");
+let button = document.getElementById("startButton");
+let record = document.getElementById("record");
+let tries = document.getElementById("tries");
 
 let audio = new Audio('https://share.schneider.today/beep.mp3');
 
-function showTime(){
+function showTime() {
 
-    if(s == 0){
+    if (s == 0) {
         m--;
     }
     s = (s == 0 ? 59 : --s);
-    
+
     let time = (m < 10 ? "0" : "") + m + ":" + (s < 10 ? "0" : "") + s;
     clock.innerText = time;
     clock.textContent = time;
-    
-    if(s == 0 && m == 0){
+
+    if (s == 0 && m == 0) {
         wrapper.classList.remove("action");
         wrapper.classList.remove("finish");
         wrapper.classList.toggle("done");
-        display.innerText = "You did it!";
-        display.textContent = "You did it!";
-    }else if(s == 15 && m == 0){
+        text.innerText = "You did it!";
+        text.textContent = "You did it!";
+    } else if (s == 15 && m == 0) {
         wrapper.classList.remove("action");
         wrapper.classList.add("finish");
-        display.innerText = "Last 15 Seconds!";
-        display.textContent = "Last 15 Seconds!";
+        text.innerText = "Last 15 Seconds!";
+        text.textContent = "Last 15 Seconds!";
         audio.play();
-    }else if(s == 15){
+    } else if (s == 15) {
         wrapper.classList.toggle("action");
-        display.innerText = "Break";
-        display.textContent = "Break";
+        text.innerText = "Break";
+        text.textContent = "Break";
         audio.play();
-    }else if(s == 0){
+    } else if (s == 0) {
         wrapper.classList.toggle("action");
-        display.innerText = "Action!";
-        display.textContent = "Action!";
+        text.innerText = "Action!";
+        text.textContent = "Action!";
         audio.play();
     }
-    
-    if(m != 0 || s != 0){
-        setTimeout(showTime, 1000);
-    }
-    
 
+    if (m == 0 || s == 0) {
+        clearInterval(timer);
+        surrender();
+    }
 }
 
-function start(){
+function start() {
     button.style.display = "none";
-    if(s > 15 || s == 0){
-        display.innerText = "Action!";
-        display.textContent = "Action!";
-    }else{
+    display.style.display = "block";
+    if (s > 15 || s == 0) {
+        text.innerText = "Action!";
+        text.textContent = "Action!";
+    } else {
         wrapper.classList.remove("action");
-        display.innerText = "Break";
-        display.textContent = "Break";
+        text.innerText = "Break";
+        text.textContent = "Break";
     }
-
     showTime();
+    timer = setInterval(showTime, 1000);
+}
+
+function surrender() {
+    clearInterval(timer);
+    let seconds = (m_start * 60 + s_start) - (m * 60 + s);
+    let tries = (localStorage.getItem('tries') ? JSON.parse(localStorage.getItem('tries')) : [])
+    tries.push({
+        'date': Date.now(),
+        'seconds': seconds
+    });
+    localStorage.setItem('tries', JSON.stringify(tries));
+    let recordTime = Math.max.apply(null, tries.map(x => x.seconds));
+    localStorage.setItem('record', recordTime);
+    record.innerHTML = "Your record is " + Math.floor(recordTime / 60) + " minutes and " + recordTime % 60 + " seconds!"
+    display.style.display = "none";
+    record.style.display = "block";
+}
+
+function showPedestal() {
+    if(tries.style.display == "block"){
+        tries.style.display = "none"
+        startButton.style.display = "block";
+        m = m_start;
+        s = s_start
+
+        while (tries.firstChild) {
+            tries.removeChild(tries.firstChild);
+        }
+    }else{
+        display.style.display = "none";
+        record.style.display = "none";
+        startButton.style.display = "none";
+        tries.style.display = "block";
+    
+        JSON.parse(localStorage.getItem('tries')).forEach(element => {
+            console.log(element)
+            let p = document.createElement("p");
+            let date = new Date(element.date)
+            p.textContent = date.getDay() + "." + date.getMonth() + "." + date.getFullYear() + " - " +  (Math.floor(element.seconds / 60) < 10 ? '0' : Math.floor(element.seconds / 60)) + ":" + (element.seconds % 60 < 10 ? '0' : '') + element.seconds % 60;
+            tries.appendChild(p);
+        });
+    }
 }
